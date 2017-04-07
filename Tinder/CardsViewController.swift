24a4ae2dfb8837872d9b2cab63a177d56e867f5c
Satyam Jaiswal <snjaiswa@asu.edu>
2017-04-06 19:48:38 -0700

    Rotation on Drag and Final Card Animation

--- Tinder/CardsViewController.swift
+++ Tinder/CardsViewController.swift
@@ -22,6 +22,7 @@ class CardsViewController: UIViewController {
     }
 
     fileprivate func setupUI(){
+        print("card center y: \(self.cardImageView.center.y)")
         self.cardInitialCenter = CGPoint(x: self.cardImageView.center.x, y: self.cardImageView.center.y)
         
         self.cardImageView.layer.cornerRadius = 5
@@ -40,24 +41,32 @@ class CardsViewController: UIViewController {
     
     func didPanCard(_ sender: UIPanGestureRecognizer) {
         if let cardImage = sender.view as? UIImageView{
-            // let location = sender.location(in: view)
+             let location = sender.location(in: view)
             // let velocity = sender.velocity(in: view)
             let translation = sender.translation(in: view)
             print(translation.x)
             let angle = Double(30*translation.x/200)
-            let rotation = CGFloat( angle * M_PI / 180)
+            var rotation = CGFloat( angle * M_PI / 180)
             
             if sender.state == .began {
                 //            print("Gesture began")
 //                cardImage.center = CGPoint(x:cardImage.center.x + translation.x, y:cardImage.center.y + translation.y)
             } else if sender.state == .changed {
                 //            print("Gesture is changing")
+                var buffer: CGFloat = 50
+                if translation.x<0{
+                    buffer = -50
+                }
+                
+                if location.y > self.cardInitialCenter.y {
+                    rotation = 0-rotation
+                }
                 UIView.animate(withDuration: 1, animations: {
-                    cardImage.center = CGPoint(x:cardImage.center.x + translation.x, y:cardImage.center.y + translation.y)
+                    cardImage.center = CGPoint(x:cardImage.center.x + translation.x + buffer, y:cardImage.center.y + translation.y)
                     cardImage.transform = cardImage.transform.rotated(by: rotation)
                 })
             } else if sender.state == .ended {
-                if translation.x > -150 && translation.x < 150  {
+                if translation.x > -50 && translation.x < 50  {
                     UIView.animate(withDuration: 0.75, animations: {
                         self.cardImageView.transform = CGAffineTransform.identity
                         self.cardImageView.center = self.cardInitialCenter
